0870f797002394fcfdf0b17707ccdbaa802666d2
Nate Berol <nateberol@Nates-MacBook-Pro.local>
2024-07-09 11:02:40 -0700

    Added new level

---
+++ challenges/overthewire-bandit/Level4/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Write the flag to a hidden file
+mkdir /home/challengeuser/inhere && \
+
+for i in {00..09}; do
+    # Construct the filename
+    filename="-file$i"
+    # Create an empty file with the constructed filename
+    base64 /dev/urandom | head -c 1000 > "./inhere/$filename"
+done
+
+echo $FLAG > /home/challengeuser/inhere/'-file07'
+
+# Prevent use of debuggers to pull env value (hopefully)
+unset FLAG
+
+./.cleanup.sh
---
+++ challenges/overthewire-bandit/Level4/readme
@@ -0,0 +1,5 @@
+Level Goal
+The password for the next level is stored in the only human-readable file in the inhere directory. Tip: if your terminal is messed up, try the “reset” command.
+
+Commands you may need to solve this level
+ls , cd , cat , file , du , find
